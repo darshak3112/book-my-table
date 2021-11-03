93593f575cfdc5efc0382fcc768afc31afc7c6ce
darshak3112 <darshakkakadiya8511@gmail.com>
2021-11-03 10:47:29 +0530

    database connected

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb+srv://darshak:darshak@cls-bmt.ofl0d.mongodb.net/test"
+
+const connetToMongo =()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connect successfully");
+    })
+}
+
+module.exports = connetToMongo;
