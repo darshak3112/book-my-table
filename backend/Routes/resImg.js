2bffcf9465a817be4caab9ab210b07373d344bf4
darshak3112 <darshakkakadiya8511@gmail.com>
2021-12-15 13:07:56 +0530

    some bug fixes

--- backend/Routes/resImg.js
+++ backend/Routes/resImg.js
@@ -1,10 +1,11 @@
 const upload = require("../middleware/upload");
 const express = require("express");
 const router = express.Router();
+const image = require("../models/Image")
 
 router.post("/upload", upload.single("file"), async (req, res) => {
     if (req.file === undefined) return res.send("you must select a file.");
-    const imgUrl = `http://localhost:8080/file/${req.file.filename}`;
+    const imgUrl = `http://localhost:5000/api/img/${req.file.filename}`;
     return res.send(imgUrl);
 });
 
