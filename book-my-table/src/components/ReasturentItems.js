26dbd5b894fbcf764d510cd3e50666d0fce0e63e
PrinceBeladiya <princebeladiya007@gmail.com>
2022-01-22 10:30:38 +0530

    footer bug solved

--- book-my-table/src/components/ReasturentItems.js
+++ book-my-table/src/components/ReasturentItems.js
@@ -37,7 +37,7 @@ export const ReasturentItems = () => {
                 </div>
             </div>
 
-            <div className="card border-dark my-2" style={{ border: "light" }}>
+            <div className="card border-dark my-2 " style={{ border: "light"}}>
                 <div className="row g-0">
                     <div className="col-md-4">
                         <center><img src={image1} className="img-fluid rounded-start" alt="..." style={{ height: "230px", width: "300px" }} /></center>
@@ -67,6 +67,7 @@ export const ReasturentItems = () => {
                     </div>
                 </div>
             </div>
+            <div style={{height:"40px"}}></div>
         </>
     );
 };
