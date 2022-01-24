f2124d6e153fb0d359fc8cdc6c233a0be95e72c9
PrinceBeladiya <princebeladiya007@gmail.com>
2022-01-24 16:59:29 +0530

    complete image upload feature

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,19 @@
 const connetToMongo = require('./db');
 const cors = require('cors')
 require("dotenv").config();
-const fileRoutes = require('./routes/file-upload-routes');
 const express = require("express");
 const app = express();
 
 connetToMongo();
-let gfs;
 const port = 5000
 
 app.use(cors())
 app.use(express.json());
 
-// //availabel routes 
-app.use('/api', fileRoutes.routes);
+app.use("/file", require("./routes/upload"));
 app.use('/api/userAuth', require('./routes/userAuth'))
 app.use('/api/vendorAuth', require('./routes/vendorAuth'))
 app.use('/api/restaurent',require('./routes/restaurent'))
-
 
 app.listen(port, () => {
   console.log(`BookMyTable app listening at http://localhost:${port}`)
