32f06cd0f5d43e0f0a9f18529abadb9bf6cd5a16
PrinceBeladiya <princebeladiya007@gmail.com>
2022-01-26 14:57:44 +0530

    some change

--- book-my-table/src/components/ReasturentItems.js
+++ book-my-table/src/components/ReasturentItems.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 
 export const ReasturentItems = (props) => {
-    const {RestItem} = props;
-    
+    const { RestItem } = props;
+
     return (
         <>
-            <div className="card border-dark my-2" style={{border: "light" }}>
+            <div className="card border-dark my-2" style={{ border: "light" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
                         <center><img src={image1} className="img-fluid rounded-start" alt="..." style={{ height: "230px", width: "300px" }} /></center>
@@ -21,20 +21,23 @@ export const ReasturentItems = (props) => {
                                 </div>
                                 <div className="col-3">
                                     <div style={{ textAlign: "end" }}>
-                                        <span className={`badge bg-${RestItem.Active?"success":"danger"}`}>{RestItem.Active?"Active":"Pading"}</span>
+                                        <span className={`badge bg-${RestItem.Active ? "success" : "danger"}`}>{RestItem.Active ? "Active" : "Pading"}</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className="card-text">Food Type: {RestItem.FoodType}<br />
-                                FoodCategory:{RestItem.FoodCategory}<br/>
-                                Facility:Entrance Lobby,Queue,Serving Area,Cashier Station,Dining<br />
-                                Opening Time: {RestItem.TimeOpen}<br />
-                                Cloding Time: {RestItem.TimeClose}<br/>
-                                Table require: {RestItem.Table_require}<br />
-                                Address: {RestItem.City},{RestItem.Area}<br />
-                                Contact No : {RestItem.Contact}<br/>
-                                Holiday: {RestItem.Holiday}<br/>
-                                <Link type="button" className="btn btn-dark my-2" to="/tablebooking">Book Table</Link>
+                            <div className="card-text">
+                                <table>
+                                    <tr><td><b> Food Type :</b> {RestItem.FoodType}</td>
+                                        <td><div style={{marginLeft:"10px"}}><b> FoodCategory : </b>{RestItem.FoodCategory}</div></td></tr>
+                                    <tr><td><b> Facility : </b>Entrance Lobby,Queue,Serving Area,Cashier Station,Dining</td>
+                                        <td><div style={{marginLeft:"10px"}}><b> Opening Time :</b> {RestItem.TimeOpen}</div></td></tr>
+                                    <tr><td><b> Cloding Time :</b> {RestItem.TimeClose}</td>
+                                        <td><div style={{marginLeft:"10px"}}><b> Table require :</b> {RestItem.Table_require}</div></td></tr>
+                                    <tr><td><b> Address :</b> {RestItem.City},{RestItem.Area}<br /></td>
+                                        <td><div style={{marginLeft:"10px"}}><b> Contact No :</b> {RestItem.Contact}</div></td></tr>
+                                    <tr><td><b> Holiday :</b> {RestItem.Holiday}</td></tr>
+                                    <tr><td><Link type="button" className="btn btn-dark my-2" to="/tablebooking">Book Table</Link></td></tr>
+                                </table>
                             </div>
                         </div>
                     </div>
