74690e6502a2661ff03caf1e109de25a9d4f076c
Rihil sanghani <rihilsanghani@gmail.com>
2022-02-02 21:23:57 +0530

    Some errors is solved

--- book-my-table/src/components/SignIn.js
+++ book-my-table/src/components/SignIn.js
@@ -45,17 +45,17 @@ const SignIn = () => {
                                     <img width="150px" src={image1} alt="..." />
                                 </center>
                                 <center><h1>User SignIn</h1></center>
-                                <label for="exampleInputPhone" className="form-label">Phone No</label>
+                                <label htmlFor="exampleInputPhone" className="form-label">Phone No</label>
                                 <input type="text" className="form-control" id="exampleInputPhone" name="Mobile_no" onChange={onChange} placeholder='Enter phone No' />
                                 <div id="emailHelp" className="form-text">We'll never share your Phone No with anyone else.</div>
                             </div>
                             <div className="mb-3">
-                                <label for="exampleInputPassword1" className="form-label">Password</label>
+                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                                 <input type="password" className="form-control" id="exampleInputPassword1" name="Password" onChange={onChange} placeholder='Password' />
                             </div>
                             <center><button type="submit" className="btn btn-primary">Submit</button></center>
                         </form>
-                        <center><label style={{ marginTop: "5px" }} for="signin" className="form-label">Don't have the account? </label>
+                        <center><label style={{ marginTop: "5px" }} htmlFor="signin" className="form-label">Don't have the account? </label>
                             <Link className="card-link my-2" to="/Signup"> Sign Up</Link></center>
                     </div>
                 </div>
