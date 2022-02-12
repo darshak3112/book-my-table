79842caeacdef9b9d3d7e06fdf9b889d6fb76947
darshak3112 <darshakkakadiya8511@gmail.com>
2022-02-12 10:34:50 +0530

    some changes in backend

--- backend/middleware/fetchtable.js
+++ backend/middleware/fetchtable.js
@@ -2,17 +2,17 @@ var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const fetchuser = (req, res, next) => {
+const fetchtable = (req, res, next) => {
     // Get the vendor from the jwt token and add id to req object
-    const token = req.header('auth-token-user');
+    const token = req.header('auth-token-res');
     if (!token) {
         res.status(401).send({ error: "Please authenticate using a valid token " })
     }
-   // console.log(data.user)
+   // console.log(data.table)
     try {
         const data = jwt.verify(token, JWT_SECRET);
         console.log("this is data   "+data._id)
-        req.user = data.user;
+        req.table = data.table;
         next();
     } catch (err) {
         res.status(401).send({ error: err })
@@ -20,4 +20,4 @@ const fetchuser = (req, res, next) => {
 
 }
 
-module.exports = fetchuser;
+module.exports = fetchtable;
--- backend/models/Table.js
+++ backend/models/Table.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const Restaurant_informationSchema = new Schema({
+
+const TableSchema = new Schema({
     Restaurant: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Restaurent_information'
@@ -9,12 +10,9 @@ const Restaurant_informationSchema = new Schema({
     NumOfTable: {
         type: Int32,
         require: true
-    },
-    TimeZone: {
-        
     }
     
 });
 
-const Restaurant_information = mongoose.model('Restaurant_information', Restaurant_informationSchema);
-module.exports = Restaurant_information;
+const Table = mongoose.model('Table', TableSchema);
+module.exports = Table;
