5f15ba26bd5bbe7ce82beeb897c36621a049d7d6
PrinceBeladiya <princebeladiya007@gmail.com>
2022-02-19 10:17:30 +0530

    changes

---
+++ backend/Routes/admin.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { body, validationResult, sanitize } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+require('dotenv').config()
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// ROUTE 2: Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+  body('Mobile_no', 'Enter a valid mobile number').isLength({ min: 3 }),
+  body('Password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    let success = false;
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { Mobile_no, Password } = req.body;
+    try {
+        let user = await User.findOne({ Mobile_no });
+        if (!user) {
+            success = false
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+
+        const passwordCompare = await bcrypt.compare(Password, user.Password);
+        if (!passwordCompare) {
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct credentials" });
+        }
+
+        const data = {
+            user: {
+                id: user._id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        success = true;
+        res.status(200).json({ success, authtoken })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+module.exports = router
