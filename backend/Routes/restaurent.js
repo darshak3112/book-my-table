b4c80f63d109faa9b299c1a9fd1c76bdfffe4d5c
darshak3112 <darshakkakadiya8511@gmail.com>
2022-02-19 14:17:14 +0530

    some changes

--- backend/Routes/restaurent.js
+++ backend/Routes/restaurent.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const fetchvendor = require('../middleware/fetchvendor');
-const fetchtable = require('../middleware/fetchtable')
 const Restaurant = require('../models/Restaurant_information');
-const Table = require('../models/Table');
-const jwt = require("jsonwebtoken");
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET;
 
 // add restaurent details login required
-
 router.post('/addres', fetchvendor, [
     body('Name', 'name should be atlest 2 char').isLength({ min: 2 }),
     body('City', 'name should be atlest 2 char').isLength({ min: 2 }),
@@ -35,18 +30,6 @@ router.post('/addres', fetchvendor, [
         })
         const savedRes = await restaurant.save();
 
-        const data = {
-            restaurant: {
-                id: restaurant._id
-            }
-        }
-
-        const authtoken = jwt.sign(data, JWT_SECRET);
-
-
-        // res.json(user)
-        res.json({ authtoken })
-
         res.json(savedRes);
         console.log(savedRes.id)
     } catch (error) {
@@ -55,36 +38,6 @@ router.post('/addres', fetchvendor, [
     }
 })
 
-//add table
-router.post('/addtable', fetchtable, async (req, res) => {
-    try {
-        const { Person } = req.body;
-        const errors = validationResult(req);
-        
-        const table = new Table({
-            Person, Restaurant: req.restaurant.id
-        })
-        const savedTable = await table.save();
-
-        const data = {
-            table: {
-                id: table._id
-            }
-        }
-
-        const authtoken = jwt.sign(data, JWT_SECRET);
-
-
-        // res.json(user)
-        res.json({ authtoken })
-
-        res.json(savedTable);
-        console.log(savedTable.id)
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("some error occured");
-    }
-})
 
 
 //get all restaurent to a perticular user
@@ -101,7 +54,7 @@ router.get('/fetchallres', fetchvendor, async (req, res) => {
 //update restaurent information
 router.patch('/updateres/:id', fetchvendor, async (req, res) => {
     try {
-        const { Name, City, Area, FoodCategory, FoodType, Address, TimeOpen, TimeClose, Contact, Facility, Holiday, Table_require } = req.body;
+        const { Name, City, Area,FoodCategory,FoodType, TimeOpen, TimeClose, Contact, Facility, Holiday,Active, Table_require } = req.body;
         //new object
         const newRes = {};
         if (Name) { newRes.Name = Name };
@@ -109,7 +62,6 @@ router.patch('/updateres/:id', fetchvendor, async (req, res) => {
         if (Area) { newRes.Area = Area };
         if (FoodCategory) { newRes.FoodCategory = FoodCategory };
         if (FoodType) { newRes.FoodType = FoodType };
-        if (Address) { newRes.Address = Address };
         if (TimeOpen) { newRes.TimeOpen = TimeOpen };
         if (TimeClose) { newRes.TimeClose = TimeClose };
         if (Contact) { newRes.Contact = Contact };
@@ -140,15 +92,15 @@ router.delete('/deleteres/:id', fetchvendor, async (req, res) => {
     try {
 
         //delete
-        let dRes = await Restaurant.findById(req.params.id);
-        if (!dRes) { return res.status(404).send("not found") }
+        let dNote = await Restaurant.findById(req.params.id);
+        if (!dNote) { return res.status(404).send("not found") }
 
-        if (dRes.Vendor.toString() !== req.vendor.id) {
+        if (dNote.Vendor.toString() !== req.vendor.id) {
             return res.status(401).send("not allowed");
         }
 
-        dRes = await Restaurant.findByIdAndDelete(req.params.id);
-        res.json({ "success": "restaurent has been deleted", dRes: dRes });
+        dNote = await Restaurant.findByIdAndDelete(req.params.id);
+        res.json({ "success": "note has been deleted", dNote: dNote });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("some error occured");
@@ -158,7 +110,7 @@ router.delete('/deleteres/:id', fetchvendor, async (req, res) => {
 // get all resturent
 router.get('/getallrest', async (req, res) => {
     try {
-        const allres = await Restaurant.find({ Active: true });
+        const allres = await Restaurant.find({Active:true});
         res.json(allres);
     } catch (error) {
         console.error(error.message);
@@ -167,5 +119,4 @@ router.get('/getallrest', async (req, res) => {
 })
 
 module.exports = router;
-
 
