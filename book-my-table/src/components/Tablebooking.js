0da510f73d6f1c4c0772f357c708c3bf997c77fa
PrinceBeladiya <princebeladiya007@gmail.com>
2022-03-01 16:52:01 +0530

    table booking changes

--- book-my-table/src/components/Tablebooking.js
+++ book-my-table/src/components/Tablebooking.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
+import "./css/Tablebooking.css";
 
 
 export const Tablebooking = () => {
+
+  const [timeArray, setTimeArray] = useState([]);
+  const [dateArray, setDateArray] = useState([]);
+  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+
   let location = useLocation();
   const myparam = location.state.RestItem;
 
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var dateObj = new Date();
   var month = dateObj.getMonth() + 1;
-  var day = dateObj.getDate();
+  var day = dateObj.getDate() - 1;
   var year = dateObj.getFullYear();
 
-  var date = day + "/" + month + "/" + year;
-  var d = days[dateObj.getDay()];
-
   let da;
+
   const getDate = () => {
     var dateObj2 = day + 1;
     dateObj.setDate(dateObj2);
@@ -28,64 +35,76 @@ export const Tablebooking = () => {
     return date2;
   }
 
-  const dates = [];
 
-  const getdates = () => {
-    dates.push(<button type="button" className="btn btn-outline-secondary btn-lg mx-2" style={{ marginTop: "10px" }}>{date}<br></br>{d}</button>);
-    for (let i = 1; i <= 6; i++) {
-      dates.push(<button type="button" className="btn btn-outline-secondary btn-lg mx-2" style={{ marginTop: "10px" }}>{getDate()}<br></br>{da}</button>);
+  useEffect(() => {
+    let timeOpenStamps = myparam.TimeOpen;
+    let timeCloseStamps = myparam.TimeClose;
+    let Onum = 0, Cnum = 0;
+    let tempArray = [];
+    let dateArray = [];
+
+    for(let i = 0; timeOpenStamps[i] !== '\0'; i++) {
+      if (timeOpenStamps[i] === "A") {
+        Onum = parseInt(timeOpenStamps);
+        break;
+      }
+      else if (timeOpenStamps[i] === "P") {
+        Onum = parseInt(timeOpenStamps) + 12;
+        break;
+      }
     }
-    return dates;
+
+    for(let i = 0; timeCloseStamps[i] !== '\0'; i++) {
+      if (timeCloseStamps[i] === "A") {
+        Cnum = parseInt(timeCloseStamps);
+        break;
+      }
+      else if (timeCloseStamps[i] === "P") {
+        Cnum = parseInt(timeCloseStamps) + 12;
+        break;
+      }
+    }
+
+
+    for(let i = Onum; i <= Cnum; i++) {
+      tempArray.push(i);
+    }
+    setTimeArray(tempArray);
+
+    for(let i = 1; i <= 7; i++) {
+      dateArray.push(i);
+    }
+    setDateArray(dateArray);
+
+  }, []);
+
+  const handleChange = (time) => {
+      setSelectedTime(time);
   }
 
-  const ti = [];
-  let timeOpenStamps = myparam.TimeOpen;
-  let timeCloseStamps = myparam.TimeClose;
-  let Onum = 0, Cnum = 0;
+  const handleDateChange = (date) => {
+    ////////// Don't Remove these code //////////
+    // var dateObj = new Date();
+    // var month = dateObj.getMonth() + 1;
+    // var day = dateObj.getDate() + (date - 1);
+    // var year = dateObj.getFullYear();
 
-  for (let i = 0; timeOpenStamps[i] !== '\0'; i++) {
-    if (timeOpenStamps[i] === "A") {
-      Onum = parseInt(timeOpenStamps);
-      break;
-    }
-    else if (timeOpenStamps[i] === "P") {
-      Onum = parseInt(timeOpenStamps) + 12;
-      break;
-    }
-  }
+    // var d = day + "/" + month + "/" + year;
+    // console.log(d);
 
-  for (let i = 0; timeCloseStamps[i] !== '\0'; i++) {
-    if (timeCloseStamps[i] === "A") {
-      Cnum = parseInt(timeCloseStamps);
-      break;
-    }
-    else if (timeCloseStamps[i] === "P") {
-      Cnum = parseInt(timeCloseStamps) + 12;
-      break;
-    }
-  }
-  
-  var Color = "outline-secondary";
-
-  const handleChange = () => {
-      Color = "secondary";
-  }
-
-  const gettimes = () => {
-    for (let i = Onum; i <= Cnum; i++) {
-      ti.push(<button type="button" className={`btn btn-${Color} btn-lg mx-2`} onClick={handleChange} style={{ marginTop: "10px", width: "100px" }}>{i} : 00</button>);
-    }
-    return ti;
-  }
+    setSelectedDate(date);
+}
 
   return ( //With changes
     <>
-    <div>
+    <div className="table-booking">
       <div className="card mx-3">
         <div className="card-body">
           <center><h1>Table Booking</h1></center><hr />
           <h5 className="card-title">Select Date</h5>
-          {getdates()}   
+          {dateArray.map((date, i) => (
+              <button type="button" className={`btn ${selectedDate == date ? 'btn-success' : 'btn-outline-secondary'} btn-lg mx-2`} onClick={() => handleDateChange(date)} style={{ marginTop: "10px", width: "130px" }}>{getDate()}<br/>{da}</button>
+          ))}
         </div>
       </div>
       <div className="card my-3 mx-3 w-50" style={{ float: "left"}}>
@@ -107,7 +126,9 @@ export const Tablebooking = () => {
         <div className="card-body">
           <h5 className="card-title">Select Time</h5>
           <h6 className="card-subtitle mb-2 text-muted">Select which time do you want</h6>
-          {gettimes()}
+          {timeArray.map((time, i) => (
+              <button type="button" className={`btn ${selectedTime == time ? 'btn-success' : 'btn-outline-secondary'} btn-lg mx-2`} onClick={() => handleChange(time)} style={{ marginTop: "10px", width: "100px" }}>{time} : 00</button>
+          ))}
         </div>
       </div>
     </div>
